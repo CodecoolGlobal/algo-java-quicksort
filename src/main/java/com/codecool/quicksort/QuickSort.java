ab17d0b1a9fb6daeeaa859ee9156ec37c0ca4aeb
Matyas Forian Szabo <forianszm@gmail.com>
2017-07-10 13:06:12 +0200

    Add initial  tests and code

---
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -0,0 +1,14 @@
+package com.codecool.quicksort;
+
+import java.util.List;
+
+public class QuickSort {
+
+    /**
+     * Sorts the given List in place
+     * @param toSort the List to sort. Throws an error if its null
+     */
+    public void sort(List<Integer> toSort) {
+
+    }
+}
